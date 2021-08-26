87a4b9c45a0f2375c73ba7480edb3895d6156fba
Richard Chien <richardchienthebest@gmail.com>
2021-08-27 00:25:29 +0800

    feat(event): use RWMutex to protect EventEmitter

--- event/emitter.go
+++ event/emitter.go
@@ -2,12 +2,14 @@ package event
 
 import (
 	"encoding/json"
+	"sync"
 
 	log "github.com/sirupsen/logrus"
 )
 
 type EventEmitter struct {
-	outChans []chan []byte
+	outChans     []chan []byte
+	outChansLock sync.RWMutex
 }
 
 func NewEventEmitter() *EventEmitter {
@@ -17,12 +19,18 @@ func NewEventEmitter() *EventEmitter {
 }
 
 func (emitter *EventEmitter) OpenOutChan() <-chan []byte {
+	emitter.outChansLock.Lock()
+	defer emitter.outChansLock.Unlock()
+
 	outCh := make(chan []byte) // TODO: channel size
 	emitter.outChans = append(emitter.outChans, outCh)
 	return outCh
 }
 
 func (emitter *EventEmitter) CloseOutChan(outCh <-chan []byte) {
+	emitter.outChansLock.Lock()
+	defer emitter.outChansLock.Unlock()
+
 	for i, ch := range emitter.outChans {
 		if ch == outCh {
 			close(ch)
@@ -39,6 +47,9 @@ func (emitter *EventEmitter) Emit(event anyEvent) {
 		log.Warnf("事件序列化失败, 错误: %v", err)
 		return
 	}
+
+	emitter.outChansLock.RLock() // use read lock to allow emitting events concurrently
+	defer emitter.outChansLock.RUnlock()
 	for _, ch := range emitter.outChans {
 		ch <- jsonBytes
 	}
