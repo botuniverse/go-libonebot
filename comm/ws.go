57acc8455fa3d5628616ba93c0cf0dd00ab44c54
Richard Chien <richardchienthebest@gmail.com>
2021-08-26 13:08:16 +0800

    feat(comm): ws now can receive action request

--- comm/ws.go
+++ comm/ws.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/botuniverse/go-libonebot/utils"
 	"github.com/gorilla/websocket"
 	log "github.com/sirupsen/logrus"
+	"github.com/tidwall/gjson"
 )
 
 type wsComm struct {
@@ -18,28 +20,56 @@ var upgrader = websocket.Upgrader{
 }
 
 func (comm *wsComm) handle(w http.ResponseWriter, r *http.Request) {
-	log.Infof("WebSocket 请求: %v", r.URL)
+	log.Infof("收到来自 %v 的 WebSocket 连接请求", r.RemoteAddr)
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Errorf("WebSocket 连接失败: %v", err)
+		log.Errorf("WebSocket 连接失败, 错误: %v", err)
 		return
 	}
 	log.Infof("WebSocket 连接成功")
 	defer conn.Close()
-	for {
-		_, message, err := conn.ReadMessage()
-		if err != nil {
-			log.Errorf("WebSocket 接收错误: %v", err)
-			break
+
+	ws_chan := make(chan []byte) // TODO: channel size
+	go func() {
+		for {
+			_, message, err := conn.ReadMessage()
+			if err != nil {
+				if websocket.IsCloseError(err, websocket.CloseNormalClosure) {
+					log.Infof("WebSocket 连接断开")
+				} else {
+					log.Errorf("WebSocket 连接异常断开, 错误: %v", err)
+				}
+				break
+			}
+			ws_chan <- message
 		}
-		log.Infof("WebSocket 接收到消息: %v", string(message))
+	}()
+
+	for {
+		select {
+		case messageBytes := <-ws_chan:
+			message := utils.BytesToString(messageBytes)
+			log.Debugf("WebSocket message: %v", message)
+			if !gjson.Valid(message) {
+				log.Warnf("Action 请求体不是合法的 JSON, 已忽略")
+				continue
+			}
+			actionRequest := gjson.Parse(message)
+			actionResponse := handleAction(actionRequest)
+			conn.WriteMessage(websocket.TextMessage, utils.StringToBytes(actionResponse.String()))
+		}
 	}
+}
+
+func handleAction(request gjson.Result) gjson.Result {
+	// TODO: now it simply return the request
+	return request
 }
 
 // Start a WebSocket commmunication task.
 func StartWSTask(host string, port uint16) {
 	addr := fmt.Sprintf("%s:%d", host, port)
-	log.Infof("正在启动 WebSocket 通信方式, 监听地址: %v", addr)
+	log.Infof("正在启动 WebSocket (%v)...", addr)
 
 	comm := &wsComm{}
 	mux := http.NewServeMux()
@@ -47,8 +77,8 @@ func StartWSTask(host string, port uint16) {
 
 	go func() {
 		if err := http.ListenAndServe(addr, mux); err != nil && err != http.ErrServerClosed {
-			log.Errorf("WebSocket 通信方式启动失败, 错误: %v", err)
+			log.Errorf("WebSocket (%v) 启动失败, 错误: %v", addr, err)
 		}
-		log.Info("WebSocket 通信方式已退出")
+		log.Infof("WebSocket (%v) 已关闭", addr)
 	}()
 }
