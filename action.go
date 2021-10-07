e82bc10e942655a626fafa7e6d3e285404973534
Richard Chien <stdrc@outlook.com>
2021-10-07 23:59:22 +0800

    feat(action): complete standard actions

--- action.go
+++ action.go
@@ -8,7 +8,7 @@ const (
 
 	// OneBot 元信息相关动作
 	ActionGetStatus  = "get_status"  // 获取 OneBot 运行状态
-	ActionGetVersion = "get_version" // 获取 OneBot 版本
+	ActionGetVersion = "get_version" // 获取 OneBot 版本信息
 
 	// 消息相关动作
 	ActionSendMessage   = "send_message"   // 发送消息
@@ -24,6 +24,18 @@ const (
 	ActionGetGroupList       = "get_group_list"        // 获取群列表
 	ActionGetGroupMemberInfo = "get_group_member_info" // 获取群成员信息
 	ActionGetGroupMemberList = "get_group_member_list" // 获取群成员列表
+	ActionSetGroupName       = "set_group_name"        // 设置群名称
+	ActionLeaveGroup         = "leave_group"           // 退出群
+	ActionKickGroupMember    = "kick_group_member"     // 踢出群成员
+	ActionBanGroupMember     = "ban_group_member"      // 禁言群成员
+	ActionUnbanGroupMember   = "unban_group_member"    // 解除禁言群成员
+	ActionSetGroupAdmin      = "set_group_admin"       // 设置群管理员
+	ActionUnsetGroupAdmin    = "unset_group_admin"     // 取消群管理员
+
+	// 文件相关
+	ActionGetFile              = "get_file"               // 获取文件
+	ActionUploadFile           = "upload_file"            // 上传文件
+	ActionUploadFileFragmented = "upload_file_fragmented" // 分片上传文件
 )
 
 // Action 表示一个动作名称.
