f08b975bec580346835fe0e54493ab1c64037b24
Richard Chien <stdrc@outlook.com>
2021-10-08 23:06:51 +0800

    feat(message): complete standard message segment

--- message_segment.go
+++ message_segment.go
@@ -29,8 +29,15 @@ func (s Segment) MarshalMsgpack() ([]byte, error) {
 
 // SegTypeXxx 表示 OneBot 标准定义的核心消息段类型.
 const (
-	SegTypeText    = "text"    // 纯文本消息段
-	SegTypeMention = "mention" // 提及 (即 @) 消息段
+	SegTypeText       = "text"        // 纯文本消息段
+	SegTypeMention    = "mention"     // 提及 (即 @) 消息段
+	SegTypeMentionAll = "mention_all" // 提及所有人消息段
+	SegTypeImage      = "image"       // 图片消息段
+	SegTypeVoice      = "voice"       // 语音消息段
+	SegTypeVideo      = "video"       // 视频消息段
+	SegTypeFile       = "file"        // 文件消息段
+	SegTypeLocation   = "location"    // 位置消息段
+	SegTypeReply      = "reply"       // 回复消息段
 )
 
 func segmentFromMap(m map[string]interface{}) (Segment, error) {
@@ -78,14 +85,6 @@ func CustomSegment(type_ string, data map[string]interface{}) Segment {
 	}
 }
 
-// ExtendedSegment 构造一个指定类型的扩展消息段.
-func ExtendedSegment(prefix string, type_ string, data map[string]interface{}) Segment {
-	return Segment{
-		Type: prefix + "_" + type_,
-		Data: EasierMapFromMap(data),
-	}
-}
-
 // TextSegment 构造一个纯文本消息段.
 func TextSegment(text string) Segment {
 	return CustomSegment(SegTypeText, map[string]interface{}{
@@ -97,5 +96,56 @@ func TextSegment(text string) Segment {
 func MentionSegment(userID string) Segment {
 	return CustomSegment(SegTypeMention, map[string]interface{}{
 		"user_id": userID,
+	})
+}
+
+// MentionAllSegment 构造一个提及所有人消息段.
+func MentionAllSegment() Segment {
+	return CustomSegment(SegTypeMentionAll, map[string]interface{}{})
+}
+
+// ImageSegment 构造一个图片消息段.
+func ImageSegment(fileID string) Segment {
+	return CustomSegment(SegTypeImage, map[string]interface{}{
+		"file_id": fileID,
+	})
+}
+
+// VoiceSegment 构造一个语音消息段.
+func VoiceSegment(fileID string) Segment {
+	return CustomSegment(SegTypeVoice, map[string]interface{}{
+		"file_id": fileID,
+	})
+}
+
+// VideoSegment 构造一个视频消息段.
+func VideoSegment(fileID string) Segment {
+	return CustomSegment(SegTypeVideo, map[string]interface{}{
+		"file_id": fileID,
+	})
+}
+
+// FileSegment 构造一个文件消息段.
+func FileSegment(fileID string) Segment {
+	return CustomSegment(SegTypeFile, map[string]interface{}{
+		"file_id": fileID,
+	})
+}
+
+// LocationSegment 构造一个位置消息段.
+func LocationSegment(latitude float64, longitude float64, title string, content string) Segment {
+	return CustomSegment(SegTypeLocation, map[string]interface{}{
+		"latitude":  latitude,
+		"longitude": longitude,
+		"title":     title,
+		"content":   content,
+	})
+}
+
+// ReplySegment 构造一个回复消息段.
+func ReplySegment(messageID string, userID string) Segment {
+	return CustomSegment(SegTypeReply, map[string]interface{}{
+		"message_id": messageID,
+		"user_id":    userID,
 	})
 }
